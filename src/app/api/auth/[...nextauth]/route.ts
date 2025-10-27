78f1a84f1c35ac40021a07bdef39380948bc088e
Safth-d <fatahoudabre@live.fr>
2025-10-27 01:46:46 +0000

    Optimisation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth, { AuthOptions, Session, JWT } from "next-auth";
+import NextAuth, { AuthOptions, Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
