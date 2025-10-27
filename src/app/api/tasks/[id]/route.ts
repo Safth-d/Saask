ee3b2f5aeb8a39208230dc5c8bf246778de0eb86
Safth-d <fatahoudabre@live.fr>
2025-10-27 09:36:26 +0000

    Optimisation

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../api/auth/[...nextauth]/route"; // Adjust path as needed
 
 export async function GET(
   request: NextRequest,
